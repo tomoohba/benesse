4e051485820f179541cdfc8086179acbd4e393d5
tomoohba <smalldot1561@gmail.com>
2023-07-01 21:08:00 +0900

    レイアウト修正

--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +11,7 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Benesse',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -31,10 +29,22 @@ class ResultHome extends StatefulWidget {
 
 class _ResultHomeState extends State<ResultHome> {
   int _grade = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  //void _pushHomePage() async {
+  //  /////////
+  //  await Navigator.of(context).push(
+  //    MaterialPageRoute(
+  //      builder: (context) {
+  //        return ; ///////遷移先のクラス
+  //      },
+  //    ),
+  //  );
+  //  setState(() {});
+  //}
+  void _ontapedbottom(int index) {
     setState(() {
-      _grade++;
+      _selectedIndex = index;
     });
   }
 
@@ -43,36 +53,101 @@ class _ResultHomeState extends State<ResultHome> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('今回の点数', style: TextStyle(fontSize: 35)),
-            Text(
-              '$_grade/10',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Container>[
+              Container(
+                height: 300.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    const Text('今回の点数',
+                        style: TextStyle(fontSize: 35, fontFamily: 'MSゴシック')),
+                    Text(
+                      '$_grade/10',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 300.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: const Text(
+                        'HOME',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(170, 100),
+                        primary: Colors.grey[300],
+                        onPrimary: Colors.purple,
+                      ),
+                      onPressed: () {},
+                    ),
+                    ElevatedButton(
+                      child: const Text(
+                        'NEXT',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(170, 100),
+                        primary: Colors.grey[300],
+                        onPrimary: Colors.purple,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ]),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 40,
-        unselectedLabelStyle: TextStyle(fontSize: 35, color: Colors.black),
-        unselectedFontSize: 45,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.next_plan,
-              color: Colors.black,
-            ),
-            label: '次の10問',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.keyboard_return),
-            label: "HOME",
-          ),
-        ],
-        backgroundColor: Colors.lightBlue,
-        onTap: (index) => debugPrint("$index"),
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: _selectedIndex,
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(
+      //         Icons.next_plan,
+      //         color: Colors.black,
+      //       ),
+      //       label: "NEXT",
+      //       //backgroundColor: Colors.blue,
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.keyboard_return, color: Colors.black),
+      //       label: "HOME",
+      //       //backgroundColor: Colors.red,
+      //     ),
+      //   ],
+
+      //   onTap: _ontapedbottom,
+      //   type: BottomNavigationBarType.fixed,
+      //   // ここで色を設定していても、shiftingにしているので
+      //   // Itemの方のbackgroundColorが勝ちます。
+      //   //backgroundColor: Colors.red,
+      //   enableFeedback: true,
+      //   // IconTheme系統の値が優先されます。
+      //   iconSize: 18,
+      //   // 横向きレイアウトは省略します。
+      //   // landscapeLayout: 省略
+      //   selectedFontSize: 20,
+      //   selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
+      //   selectedLabelStyle: const TextStyle(color: Colors.red),
+      //   // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
+      //   selectedItemColor: Colors.black,
+      //   unselectedFontSize: 20,
+      //   unselectedIconTheme: const IconThemeData(size: 25, color: Colors.white),
+      //   unselectedLabelStyle: const TextStyle(color: Colors.black),
+      //   // IconTheme系統の値が優先されるのでこの値は適応されません。
+      //   //unselectedItemColor: Colors.red,
+      // ),
     );
   }
 }
