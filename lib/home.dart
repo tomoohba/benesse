1bcae0a5357870fced9372915abc3ea724062a99
ymitsuoka2025 <ymitsuoka.2025@gmail.com>
2023-07-02 14:46:50 +0900

    ソースコードの警告の解消

--- lib/home.dart
+++ lib/home.dart
@@ -91,49 +91,56 @@ class _MyHomePageState extends State<MyHomePage> {
                     //     ),
                     //   ],
                     // ),
-                    child: Text(
-                      '英語',
-                      style: TextStyle(
-                          color: Colors.white,
-                          // fontWeight: FontWeight.bold,
-                          fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.orange[500],
                       onPrimary: Colors.orange[900],
                     ),
+                    child: const Text(
+                      '英語',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          // fontWeight: FontWeight.bold,
+                          fontSize: 22),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '数学',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.blue[500],
                       onPrimary: Colors.blue[900],
                     ),
+                    child: const Text(
+                      '数学',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '国語',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.red[500],
                       onPrimary: Colors.red[900],
                     ),
+                    child: const Text(
+                      '国語',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                 ],
               ),
@@ -148,46 +155,55 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '物理',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.purple[500],
                       onPrimary: Colors.purple[900],
                     ),
+                    child: const Text(
+                      '物理',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '化学',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.pink[500],
                       onPrimary: Colors.pink[900],
                     ),
+                    child: const Text(
+                      '化学',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '生物',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.teal[500],
                       onPrimary: Colors.teal[900],
                     ),
+                    child: const Text(
+                      '生物',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                 ],
               ),
@@ -202,45 +218,54 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '地理',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.green[500],
                       onPrimary: Colors.green[900],
                     ),
+                    child: const Text(
+                      '地理',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '日本史',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.deepOrange[500],
                       onPrimary: Colors.deepOrange[900],
                     ),
+                    child: const Text(
+                      '日本史',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '世界史',
-                      style: TextStyle(color: Colors.white, fontSize: 22),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(100, 70),
                       elevation: 10,
                       primary: Colors.brown[500],
                       onPrimary: Colors.brown[900],
+                    ),
+                    child: const Text(
+                      '世界史',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 22),
                     ),
                   ),
                 ],
@@ -256,15 +281,18 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: () {
                       // ボタンが押された時の処理
                     },
-                    child: Text(
-                      '苦手',
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                    ),
                     style: ElevatedButton.styleFrom(
                       fixedSize: Size(200, 70),
                       elevation: 10,
                       primary: Colors.redAccent,
                       onPrimary: Colors.red[900],
+                    ),
+                    child: const Text(
+                      '苦手',
+                      style: TextStyle(
+                          fontFamily: 'Noto Sans JP',
+                          color: Colors.white,
+                          fontSize: 25),
                     ),
                   ),
                 ],
